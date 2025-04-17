3fd91a4da60d145b40187dea6255e373d9940460
louisemoana <louisemoana4twork@gmail.com>
2025-04-17 11:12:25 -0300

    adicionado o formulario

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bem-vindo</title>
+</head>
+<body>
+    <?php
+    var_dump($_REQUEST);
+    echo "Bem vindo de volta!";
+    ?>
+</body>
+</html>
